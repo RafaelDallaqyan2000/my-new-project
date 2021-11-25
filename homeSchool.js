475603a9e754e45b42ea54d720248834e11d1510
RafaelDallaqyan2000 <rafaeldallakyan4@gmail.com>
2021-11-25 17:10:35 +0400

    hello world

---
+++ homeSchool.js
@@ -0,0 +1,89 @@
+
+ window.onscroll = function() {myFunction()};
+
+ var navbar = document.getElementById("navbar");
+ var sticky = navbar.offsetTop;
+ 
+ function myFunction() {
+   if (window.pageYOffset >= sticky) {
+     navbar.classList.add("sticky")
+   } else {
+     navbar.classList.remove("sticky");
+   }
+ }
+
+//  let userList = [
+//     // { 'name': "Armadon", "surname": "Makedonsky", "age": 33 },
+//     // { 'name': "Armadon", "surname": "Makedonsky", "age": 33 }
+
+// ];
+
+
+// function diplayStudnetsList() {
+//     if (userList.length > 0) {
+//         $('#navUl').empty();
+//         userList.map(item => {
+//             let listItem = document.createElement('LI');
+//             listItem.innerHTML = '<i class="fa fa-trash-o"></i>'
+//             let content = document.createTextNode(`${item.name} ${item.surname} ${item.age}`);
+//             listItem.appendChild(content);
+//             // $(document).ready(function () {
+//             //     $('li').css('color', 'red')
+
+//             // })
+//             let mainList = document.getElementById('navUl');
+//             mainList.appendChild(listItem);
+//             $('li').css({'border-bottom' : '1px solid red', "float" : "left"})
+//             $('i').css({"color" : "red"})
+
+//         })
+//     }
+// }
+
+// diplayStudnetsList();
+
+// const button = document.getElementById('button');
+// button.addEventListener('click', function () {
+//     let newUser = {};
+//     let name = $('#name').val();
+//     let surname = $('#lastName').val();
+//     let age = $('#age').val();
+//     newUser.name = name, newUser.surname = surname, newUser.age = age;
+//     userList.push(newUser);
+//     // $('#studentsList').empty();    
+//     diplayStudnetsList();
+
+// })
+
+// const delBtn = document.getElementById('delBtn');
+// delBtn.addEventListener('click', function () {
+//     let index = $('delStudent').val();
+//     userList.splice(index, 1);
+//     alert(userList.length);
+//     diplayStudnetsList();
+// })
+
+
+// let arr = [15,20,25,36,35,40];
+// function filteredArr(arr, index) {
+//     return arr.filter(function (val, i) {
+//         return i % index;
+//     })
+// }
+// console.log(filteredArr(arr, 2));
+
+
+
+// // function bujnery(val) {
+// //     return val.split("").reverse().map(function (value, i) {
+// //         if (value === "1") {
+// //             return Math.pow(2, i);
+// //         }
+// //             return 0;
+        
+// //     }).reduce(function (aggr, val) {
+// //        return aggr + val ;
+// //     }, 0);
+// // }
+
+// // alert(bujnery("100101"));
---
+++ register.js
@@ -0,0 +1,71 @@
+$(document).ready(function(){
+ window.onscroll = function() {myFunction()};
+
+ var navbar = document.getElementById("navbar");
+ var sticky = navbar.offsetTop;
+ 
+ function myFunction() {
+   if (window.pageYOffset >= sticky) {
+     navbar.classList.add("sticky")
+   } else {
+     navbar.classList.remove("sticky");
+   }
+ }
+
+ $("#button_1").click(function(){
+ let array = {
+     "firstname" : "",
+     "lastname" : "",
+     "email" : "",
+     "mobile" : "",
+     "age" : ""
+ }
+
+    let firstname = document.getElementById("studentName").value;
+    let lastName = document.getElementById("lastName").value;
+    let email = document.getElementById("Email").value;
+    let mobile = document.getElementById("phone").value;
+    let age = document.getElementById("age").value;
+
+     array.firstname = firstname;
+     array.lastname = lastName;
+     array.email = email;
+     array.mobile = mobile;
+     array.age = age;
+   
+    console.log("Arrr-->>>", array)
+    
+    $.ajax({
+      url: "http://127.0.0.1:3000/students",
+      method: 'POST',
+      data: array,
+      success: function (returnData) {
+          alert("Student is added successfully");
+      },
+      error: function (xhr, status, error) {
+          alert('Something went wrong ' + xhr.status + ' ' + xhr.statusText);
+      }
+  })
+  
+  })
+   
+let a = 5;
+while(a < 10){
+  let b = a * 2;
+  return b
+  a++
+};
+console.log((".".repeat(b)));
+   
+})
+// function selectAlert(){
+//   let option1 = document.getElementById("opt_1").value;
+//   let option2 = document.getElementsById("opt_2");
+//   let option3 = document.getElementsById("opt_3");
+//   let globalOption = document.querySelector("option");
+
+//     option1.addEventListener("click", (val) => {
+//       alert("aaaa--->", val)
+//     })
+// }
+
